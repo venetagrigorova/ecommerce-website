d05dd0e41e6bfc74bb8a53eba9a8bce141fa276d
Veneta <veneta.grigorov@gmail.com>
2025-03-05 20:00:37 +0200

    base frontend files

--- 02-backend/sprint-boot-ecommerce/src/main/java/com/shop/ecommerce/dao/ProductRepository.java
+++ 02-backend/sprint-boot-ecommerce/src/main/java/com/shop/ecommerce/dao/ProductRepository.java
@@ -3,5 +3,5 @@ package com.shop.ecommerce.dao;
 import com.shop.ecommerce.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ProductRepository extends JpaRepository<Product, Long> {
+public interface ProductRepository extends  JpaRepository<Product, Long> {
 }
